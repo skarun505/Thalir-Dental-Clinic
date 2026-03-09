62f153b75f8dc81b097829d2e72c5e1e36b219b9
skarun505 <skarunkumar460@gmail.com>
2026-03-09 15:32:54 +0530

    Configure base path to use root when deploying on Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Thalir-Dental-Clinic/',
+  // Use root for Vercel/Local and the repo name for GitHub Pages
+  base: process.env.VERCEL ? '/' : '/Thalir-Dental-Clinic/',
 })
